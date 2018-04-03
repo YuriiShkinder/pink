9d9138b490f37f7c45531965e9db4868af132be9
YuriiShkinder <yurkaaa96@gmail.com>
2018-04-03 16:09:19 +0300

    contact content

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::resource('/portfolios','PortfolioController',['parameters'=>['portfolios'
 
 Route::resource('/articles','ArticlesController',[ 'parameters'=>['articles'=>'alias']]);
 
-Route::get('/articles/cat/{cat_alias?}',['uses'=>'ArticlesController@index','as'=>'articlesCat']);
+Route::get('/articles/cat/{cat_alias?}',['uses'=>'ArticlesController@index','as'=>'articlesCat'])->where('cat_alias','[\w-]+');
 
 Route::resource('comment','CommentController',['only'=>['store']]);
