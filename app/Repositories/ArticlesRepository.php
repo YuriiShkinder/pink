60f0d7716566c084c52bb6b7a148d015e30465ef
YuriiShkinder <yurkaaa96@gmail.com>
2018-04-06 08:41:08 +0300

    end egit asticles

--- app/Repositories/ArticlesRepository.php
+++ app/Repositories/ArticlesRepository.php
@@ -17,68 +17,128 @@ class ArticlesRepository extends Repository
 {
     public function __construct(Article $articles)
     {
-        $this->model=$articles;
+        $this->model = $articles;
     }
 
-    public function one($alias,$attr=[]){
-        $articles=parent::one($alias,$attr);
-        if($articles && ! empty($attr)){
+    public function one($alias, $attr = [])
+    {
+        $articles = parent::one($alias, $attr);
+        if ($articles && !empty($attr)) {
 
             $articles->load('comments');
             $articles->comments->load('user');
         }
-return $articles;
+        return $articles;
     }
 
-    public function addArticle($request){
-        if(Gate::denies('save',$this->model)){
+    public function addArticle($request)
+    {
+        if (Gate::denies('save', $this->model)) {
             abort(403);
         }
 
-        $data=$request->except('_token','image');
+        $data = $request->except('_token', 'image');
 
 
-        if(empty($data)){
-            return ['error'=>'нет данных'];
+        if (empty($data)) {
+            return ['error' => 'нет данных'];
         }
 
-        if(empty($data['alias'])){
-            $data['alias']= $this->transliterate($data['title']);
+        if (empty($data['alias'])) {
+            $data['alias'] = $this->transliterate($data['title']);
         }
-        if($this->one($data['alias'])){
-            $request->merge(['alias'=>$data['alias']]);
+        if ($this->one($data['alias'])) {
+            $request->merge(['alias' => $data['alias']]);
             $request->flash();
-            return ['error'=>'Занято'];
+            return ['error' => 'Занято'];
         }
 
-        if($request->hasFile('image')){
-            $image= $request->file('image');
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
 
-            if($image->isValid()){
-                $str=str_random(8);
-                $obj=new \stdClass();
-                $obj->mini=$str.'_mini.jpg';
-                $obj->max=$str.'_max.jpg';
-                $obj->path=$str.'.jpg';
+            if ($image->isValid()) {
+                $str = str_random(8);
+                $obj = new \stdClass();
+                $obj->mini = $str . '_mini.jpg';
+                $obj->max = $str . '_max.jpg';
+                $obj->path = $str . '.jpg';
 
-                $img= Image::make($image);
+                $img = Image::make($image);
 
 
-                $img->fit(Config::get('settings.image')['width'],Config::get('settings.image')['heigth'])
-                    ->save(public_path().'/'.env('THEME').'/images/articles/'.$obj->path);
-                $img->fit(Config::get('settings.articles_img')['max']['width'],Config::get('settings.articles_img')['max']['heigth'])
-                    ->save(public_path().'/'.env('THEME').'/images/articles/'.$obj->max);
-                $img->fit(Config::get('settings.articles_img')['mini']['width'],Config::get('settings.articles_img')['mini']['heigth'])
-                    ->save(public_path().'/'.env('THEME').'/images/articles/'.$obj->mini);
+                $img->fit(Config::get('settings.image')['width'], Config::get('settings.image')['heigth'])
+                    ->save(public_path() . '/' . env('THEME') . '/images/articles/' . $obj->path);
+                $img->fit(Config::get('settings.articles_img')['max']['width'], Config::get('settings.articles_img')['max']['heigth'])
+                    ->save(public_path() . '/' . env('THEME') . '/images/articles/' . $obj->max);
+                $img->fit(Config::get('settings.articles_img')['mini']['width'], Config::get('settings.articles_img')['mini']['heigth'])
+                    ->save(public_path() . '/' . env('THEME') . '/images/articles/' . $obj->mini);
 
-                $data['img']=json_encode($obj);
+                $data['img'] = json_encode($obj);
                 $this->model->fill($data);
 
-                if($request->user()->articles()->save($this->model)){
-                    return ['status'=>'Материал добавлен'];
+                if ($request->user()->articles()->save($this->model)) {
+                    return ['status' => 'Материал добавлен'];
                 }
             }
         }
     }
 
+    public function updateArticle($request, $article)
+    {
+        if (Gate::denies('edit', $this->model)) {
+            abort(403);
+        }
+
+        $data = $request->except('_token', 'image','_method','old_image');
+
+
+
+        if (empty($data)) {
+            return ['error' => 'нет данных'];
+        }
+
+        if (empty($data['alias'])) {
+            $data['alias'] = $this->transliterate($data['title']);
+        }
+        $result=$this->one($data['alias']);
+
+        if (isset($result) && ($result->id !== $article->id)) {
+            $request->merge(['alias' => $data['alias']]);
+            $request->flash();
+            return ['error' => 'Занято'];
+        }
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+
+            if ($image->isValid()) {
+                $str = str_random(8);
+                $obj = new \stdClass();
+                $obj->mini = $str . '_mini.jpg';
+                $obj->max = $str . '_max.jpg';
+                $obj->path = $str . '.jpg';
+
+                $img = Image::make($image);
+
+
+                $img->fit(Config::get('settings.image')['width'], Config::get('settings.image')['heigth'])
+                    ->save(public_path() . '/' . env('THEME') . '/images/articles/' . $obj->path);
+                $img->fit(Config::get('settings.articles_img')['max']['width'], Config::get('settings.articles_img')['max']['heigth'])
+                    ->save(public_path() . '/' . env('THEME') . '/images/articles/' . $obj->max);
+                $img->fit(Config::get('settings.articles_img')['mini']['width'], Config::get('settings.articles_img')['mini']['heigth'])
+                    ->save(public_path() . '/' . env('THEME') . '/images/articles/' . $obj->mini);
+
+                $data['img'] = json_encode($obj);
+
+            }
+
+        }
+        $article->fill($data);
+
+        if ($article->update()) {
+            return ['status' => 'Материал обновлен'];
+        }
+    }
 }
+
+
